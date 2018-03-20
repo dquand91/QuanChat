e10486295c715547f5c2541d123635f0ae5ae34f
dquand91 <dquand91@gmail.com>
2018-03-20 14:29:39 +0700

    - Show welcome page in 3s after that go to MainPage

--- app/src/main/java/luongduongquan/com/quanchat/WelcomeActivity.java
+++ app/src/main/java/luongduongquan/com/quanchat/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package luongduongquan.com.quanchat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class WelcomeActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+
+		Thread thread = new Thread(){
+			@Override
+			public void run() {
+				try{
+					sleep(3000);
+				}
+
+				catch (Exception e){
+					Log.e("ERROR", ""+ e.getMessage());
+				}
+				finally {
+					Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+					startActivity(intent);
+				}
+			}
+		};
+		thread.start();
+
 	}
 }

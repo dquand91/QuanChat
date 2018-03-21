2713a4a345191fefdf3fda94f05a8f4367914c49
dquand91 <dquand91@gmail.com>
2018-03-21 10:53:28 +0700

    - Ui Login page, SignUp page.

--- app/src/main/java/luongduongquan/com/quanchat/StartPageActivity.java
+++ app/src/main/java/luongduongquan/com/quanchat/StartPageActivity.java
@@ -1,13 +1,38 @@
 package luongduongquan.com.quanchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class StartPageActivity extends AppCompatActivity {
+public class StartPageActivity extends AppCompatActivity implements View.OnClickListener {
+
+	Button btnHadAccount;
+	Button btnNewAccount;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start_page);
+
+		btnHadAccount = findViewById(R.id.btnHadAccount);
+		btnNewAccount = findViewById(R.id.btnNeedNewAccount);
+
+		btnHadAccount.setOnClickListener(this);
+		btnNewAccount.setOnClickListener(this);
+
+
+
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()){
+			case R.id.btnHadAccount:
+				break;
+			case R.id.btnNeedNewAccount:
+				break;
+		}
+
 	}
 }

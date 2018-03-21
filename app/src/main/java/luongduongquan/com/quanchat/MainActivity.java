9085282436e766d8cc77258d6d44a0288888d3f3
dquand91 <dquand91@gmail.com>
2018-03-21 16:06:26 +0700

    - Menu. - Logout

--- app/src/main/java/luongduongquan/com/quanchat/MainActivity.java
+++ app/src/main/java/luongduongquan/com/quanchat/MainActivity.java
@@ -3,6 +3,9 @@ package luongduongquan.com.quanchat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,12 +14,18 @@ public class MainActivity extends AppCompatActivity {
 
 	private FirebaseAuth mAuth;
 
+	private Toolbar mToolbar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		mAuth = FirebaseAuth.getInstance();
+
+		mToolbar = findViewById(R.id.appbar_main);
+		setSupportActionBar(mToolbar);
+		getSupportActionBar().setTitle("myChatApp");
 	}
 
 	@Override
@@ -26,11 +35,34 @@ public class MainActivity extends AppCompatActivity {
 		FirebaseUser currentUser = mAuth.getCurrentUser();
 
 		if(currentUser == null){
-			Intent intentStartPage = new Intent(MainActivity.this, StartPageActivity.class);
-			intentStartPage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);  // After start other Activity Do not allow user press back to back this Main Activity
-			startActivity(intentStartPage);
-			finish();
+			LogoutUser();
 		}
 	}
 
+	private void LogoutUser() {
+		Intent intentStartPage = new Intent(MainActivity.this, StartPageActivity.class);
+		intentStartPage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);  // After start other Activity Do not allow user press back to back this Main Activity
+		startActivity(intentStartPage);
+		finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+
+		if(item.getItemId() == R.id.logout_menu_main){
+			mAuth.signOut();
+			LogoutUser();
+		} else if (item.getItemId() == R.id.acc_settings_menu_main){
+
+		}
+		return true;
+	}
 }
